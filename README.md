bafc01699e7de3af713d7051242d640dcc50c0d1
Dejan Jakovchevski <dexapage@gmail.com>
2019-10-25 01:04:33 +0200

    Adding Class Diagram, Read me file, and category refactor

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# TodoList
-Simple backend TodoList
+# Todo List
+A simple backend logic **Todo List**  application written in Java. This application allows you to create new tasks, by adding title, short description, due date, and choose a project for that task to belong to. You can use the application through a text-based interface via command-line. The application allows you to display, add, edit, mark as done or remove tasks. You can also quit and save the current task list to file, and then restart the application with the former state restored.
+
+## ✔️  Features
+* Add new task
+* Update task
+* Remove task
+* Mark task as done
+* Display and sort the list of tasks by due date or project 
+   (Ascending/Depending)
+* Save and load the tasks list to a file 
+
+![](WelcomeScreen.png)
+
+## User manual
+This guide will help you learn the functions of very basic **To-do List** application and get started with a simple task management system to stay organised. 
+Ones the program run, a **Main menu** is shown as below:
+
+![](MainMenu.png)
+
+By typing one of the given options you can manipulate your tasks.
+```python
+No.0 -> Save and Quit
+``` 
+You can save the current task list to file and quit the application.
+```python
+No.1 -> Show  a list of existing tasks
+```
+By entering choice **1** you are entering to the sub menu *listing tasks* where you can choose from the available options
+```python
+No.2 -> Add a new task 
+```
+By entering choice **2** you can create a new task record, by providing  *name* , *short description*, *project title* and *due date* for the particular task.
+
+Current requirement is that, *Due date* needs to be entered in specific format *yyyy M d*
+```python
+No.3 -> Update an existing task 
+```
+By entering choice *No.3* you will be asked to provide a name of the task you want to edit. After typing existing name you are entering to the sub menu where you can choose from the available options, change *name*, *change description*, *change project*, *change due date*  or *change status*(which swaps between two states: In progress or Done)
+```python
+No.4 -> Remove an existing task
+```
+By entering choice *No.4* you can remove task from your list by providing a name of existing task. 
+```python
+No.5 -> Query if any existing task exist in your list
+```
+By entering choice *No.5* you can find if task exist in your list by providing a name of a task. 
+```python
+No.6 -> Print a list of available actions
+```
+By entering choice *No.6* you can print the **Main menu**.
--- test/Model/ToDoListTest.java
+++ test/Model/ToDoListTest.java
@@ -14,110 +14,110 @@ import static org.junit.jupiter.api.Assertions.*;
 class ToDoListTest {
 
     private TestHelper ts;
-    private String dateTask="2019 10 6";
+    private String dateTask = "2019 10 6";
 
     @Test
     void addNewTask() {
         ts = new TestHelper();
-        ToDoList toDoList=new ToDoList();
-        LocalDate taskDate=ts.parseStringToLocalDate(dateTask);
-        Task expected=new Task("TestTask 1","Test Description1","TestCat 1",taskDate);
+        ToDoList toDoList = new ToDoList();
+        LocalDate taskDate = ts.parseStringToLocalDate(dateTask);
+        Task expected = new Task("TestTask 1", "Test Description1", "TestCat 1", taskDate);
         toDoList.addNewTask(expected);
-        Assertions.assertEquals(toDoList.getSize(),1);
+        Assertions.assertEquals(toDoList.getSize(), 1);
     }
 
     @Test
     void removeExistingTaskTest() {
-        ts=new TestHelper();
-        String taskName="TestTask 1";
-        ToDoList toDoList=ts.addTwoTasksWithDifferentNameAndDate();
-        Assertions.assertEquals(toDoList.removeTaskByName(taskName).getSize(),1);
+        ts = new TestHelper();
+        String taskName = "TestTask 1";
+        ToDoList toDoList = ts.addTwoTasksWithDifferentNameAndDate();
+        Assertions.assertEquals(toDoList.removeTaskByName(taskName).getSize(), 1);
     }
 
     @Test
     void removeNonExistingTaskTest() {
-        ts=new TestHelper();
-        String taskName="TestTask 3";
-        ToDoList toDoList=ts.addTwoTasksWithDifferentNameAndDate();
-        Assertions.assertEquals(toDoList.removeTaskByName(taskName).getSize(),2);
+        ts = new TestHelper();
+        String taskName = "TestTask 3";
+        ToDoList toDoList = ts.addTwoTasksWithDifferentNameAndDate();
+        Assertions.assertEquals(toDoList.removeTaskByName(taskName).getSize(), 2);
     }
 
     @Test
     void changeTaskName() {
-        ts=new TestHelper();
-        String oldTaskName="Task 1";
-        String newTaskName="New Task Name";
-        String dateString="2019 10 8";
-        LocalDate finalTaskDate=ts.parseStringToLocalDate(dateString);
-        List<Task> actualTaskList=new ArrayList<>();
-        List<Task> expectedTaskList=new ArrayList<>();
-        Task task1=new Task(oldTaskName, "Test Description2", "TestCat 2", finalTaskDate);
-        Task task2=new Task(newTaskName, "Test Description2", "TestCat 2", finalTaskDate);
+        ts = new TestHelper();
+        String oldTaskName = "Task 1";
+        String newTaskName = "New Task Name";
+        String dateString = "2019 10 8";
+        LocalDate finalTaskDate = ts.parseStringToLocalDate(dateString);
+        List<Task> actualTaskList = new ArrayList<>();
+        List<Task> expectedTaskList = new ArrayList<>();
+        Task task1 = new Task(oldTaskName, "Test Description2", "TestCat 2", finalTaskDate);
+        Task task2 = new Task(newTaskName, "Test Description2", "TestCat 2", finalTaskDate);
         actualTaskList.add(task1);
         expectedTaskList.add(task2);
-        ToDoList todoListExpected =new ToDoList(expectedTaskList);
-        ToDoList todoListActualinit=new ToDoList(actualTaskList);
-        todoListActualinit.changeTaskName(oldTaskName,newTaskName);
-        Assertions.assertEquals(todoListExpected.getMyTasks(),todoListActualinit.getMyTasks());
+        ToDoList todoListExpected = new ToDoList(expectedTaskList);
+        ToDoList todoListActualinit = new ToDoList(actualTaskList);
+        todoListActualinit.changeTaskName(oldTaskName, newTaskName);
+        Assertions.assertEquals(todoListExpected.getMyTasks(), todoListActualinit.getMyTasks());
     }
 
     @Test
     void changeTaskDescription() {
-        ts=new TestHelper();
-        String taskName="Task 1";
-        String newDescriptionName="Description Test";
-        String dateString="2019 10 8";
-        LocalDate finalTaskDate=ts.parseStringToLocalDate(dateString);
-        List<Task> actualTaskList=new ArrayList<>();
-        List<Task> expectedTaskList=new ArrayList<>();
-        Task task1=new Task(taskName, "Test Description2", "TestCat 2", finalTaskDate);
-        Task task2=new Task(taskName, "Description Test", "TestCat 2", finalTaskDate);
+        ts = new TestHelper();
+        String taskName = "Task 1";
+        String newDescriptionName = "Description Test";
+        String dateString = "2019 10 8";
+        LocalDate finalTaskDate = ts.parseStringToLocalDate(dateString);
+        List<Task> actualTaskList = new ArrayList<>();
+        List<Task> expectedTaskList = new ArrayList<>();
+        Task task1 = new Task(taskName, "Test Description2", "TestCat 2", finalTaskDate);
+        Task task2 = new Task(taskName, "Description Test", "TestCat 2", finalTaskDate);
         actualTaskList.add(task1);
         expectedTaskList.add(task2);
-        ToDoList todoListExpected =new ToDoList(expectedTaskList);
-        ToDoList todoListActualinit=new ToDoList(actualTaskList);
-        todoListActualinit.changeTaskDescription(taskName,newDescriptionName);
-        Assertions.assertEquals(todoListExpected.getMyTasks(),todoListActualinit.getMyTasks());
+        ToDoList todoListExpected = new ToDoList(expectedTaskList);
+        ToDoList todoListActualinit = new ToDoList(actualTaskList);
+        todoListActualinit.changeTaskDescription(taskName, newDescriptionName);
+        Assertions.assertEquals(todoListExpected.getMyTasks(), todoListActualinit.getMyTasks());
     }
 
     @Test
     void changeTaskCategory() {
-        ts=new TestHelper();
-        String taskName="Task 1";
-        String newCategory="Category Test";
-        String dateString="2019 10 8";
-        LocalDate finalTaskDate=ts.parseStringToLocalDate(dateString);
-        List<Task> actualTaskList=new ArrayList<>();
-        List<Task> expectedTaskList=new ArrayList<>();
-        Task task1=new Task(taskName, "Description Test", "-bdsas", finalTaskDate);
-        Task task2=new Task(taskName, "Description Test", "Category Test", finalTaskDate);
+        ts = new TestHelper();
+        String taskName = "Task 1";
+        String newCategory = "Category Test";
+        String dateString = "2019 10 8";
+        LocalDate finalTaskDate = ts.parseStringToLocalDate(dateString);
+        List<Task> actualTaskList = new ArrayList<>();
+        List<Task> expectedTaskList = new ArrayList<>();
+        Task task1 = new Task(taskName, "Description Test", "-bdsas", finalTaskDate);
+        Task task2 = new Task(taskName, "Description Test", "Category Test", finalTaskDate);
         actualTaskList.add(task1);
         expectedTaskList.add(task2);
-        ToDoList todoListExpected =new ToDoList(expectedTaskList);
-        ToDoList todoListActualinit=new ToDoList(actualTaskList);
-        todoListActualinit.changeTaskCategory(taskName,newCategory);
-        Assertions.assertEquals(todoListExpected.getMyTasks(),todoListActualinit.getMyTasks());
+        ToDoList todoListExpected = new ToDoList(expectedTaskList);
+        ToDoList todoListActualinit = new ToDoList(actualTaskList);
+        todoListActualinit.changeTaskCategory(taskName, newCategory);
+        Assertions.assertEquals(todoListExpected.getMyTasks(), todoListActualinit.getMyTasks());
     }
 
     @Test
     void changeValidTaskDueDateTest() {
-        ts=new TestHelper();
-        String finalTaskDateString="2019 8 6";
-        LocalDate finalTaskDate=ts.parseStringToLocalDate("2019 10 8");
-        String modifiedTaskName="TestTask 1";
-        ToDoList actualToDoList=ts.addTwoTasksWithDifferentNameAndDate();//create ToDoList which will be used for testing the changeDueDate
-        Scanner scanner=new Scanner(finalTaskDateString);
-        ToDoList actual=actualToDoList.changeTaskDueDate(modifiedTaskName,scanner);
+        ts = new TestHelper();
+        String finalTaskDateString = "2019 8 6";
+        LocalDate finalTaskDate = ts.parseStringToLocalDate("2019 10 8");
+        String modifiedTaskName = "TestTask 1";
+        ToDoList actualToDoList = ts.addTwoTasksWithDifferentNameAndDate();//create ToDoList which will be used for testing the changeDueDate
+        Scanner scanner = new Scanner(finalTaskDateString);
+        ToDoList actual = actualToDoList.changeTaskDueDate(modifiedTaskName, scanner);
 
         /**create the tasks with expected date after change**/
-        LocalDate expectedTaskDate=ts.parseStringToLocalDate(finalTaskDateString);
-        List<Task> expectedTaskList=new ArrayList<>();
-        Task task1=new Task("TestTask 1", "Test Description1", "TestCat 1", expectedTaskDate);
-        Task task2=new Task("TestTask 2", "Test Description2", "TestCat 2", finalTaskDate);
+        LocalDate expectedTaskDate = ts.parseStringToLocalDate(finalTaskDateString);
+        List<Task> expectedTaskList = new ArrayList<>();
+        Task task1 = new Task("TestTask 1", "Test Description1", "TestCat 1", expectedTaskDate);
+        Task task2 = new Task("TestTask 2", "Test Description2", "TestCat 2", finalTaskDate);
         expectedTaskList.add(task1);
         expectedTaskList.add(task2);
-        ToDoList todoListExpected =new ToDoList(expectedTaskList);
+        ToDoList todoListExpected = new ToDoList(expectedTaskList);
 
-        Assertions.assertEquals(todoListExpected.getMyTasks(),actual.getMyTasks());
+        Assertions.assertEquals(todoListExpected.getMyTasks(), actual.getMyTasks());
     }
 }
